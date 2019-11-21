80c3dfbfd2346f5200c668f96260f9cde175f022
kwaku96 <kwakukusi30@outlook.com>
2019-11-21 23:32:08 +0300

    made some ui corrections

--- lib/screens/favorites_screen.dart
+++ lib/screens/favorites_screen.dart
@@ -14,6 +14,7 @@ class FavoritesScreen extends StatelessWidget {
         child: Text(
           "Youd don't have any favorites yet",
           style: Theme.of(context).textTheme.display1,
+          textAlign: TextAlign.center,
         ),
       ),
     ):ListView.builder(
